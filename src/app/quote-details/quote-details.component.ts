fe7ddab413e4eb662d27caca7a808041884cece7
Tom Chege <emdeechege@gmail.com>
2018-08-10 11:00:15 +0300

    generated goal form, linked quote details to parent quote

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+@input() quote:Quote;
+@Output() isComplete= new EventEmitter<boolean>();
+
+quoteDelete(complete:boolean) {
+  this.isComplete.emit(complete);
+}
 
   constructor() { }
 
