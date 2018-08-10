7293eaa063061ed01b0f1a0b4c89bb1f54979953
Tom Chege <emdeechege@gmail.com>
2018-08-10 11:34:19 +0300

    defined output in form component plus added submit function

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,6 +8,14 @@ import {Quote} from '../quote'
 })
 export class QuoteFormComponent implements OnInit {
 
+newQuote= new Quote(0,"","", new Date());
+
+@Output() addQuote= new EventEmitter<Quote>();
+
+submitQuote() {
+  this.addQuote.emit(this.newQuote);
+}
+
   constructor() { }
 
   ngOnInit() {
