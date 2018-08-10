59b7cd61ca4e075de0c0623174b4783a2c0adbd8
Tom Chege <emdeechege@gmail.com>
2018-08-10 11:55:18 +0300

    added form module import,page not displaying

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive ,ElementRef, Input, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  constructor(private elem: ElementRef) { }
 
-  constructor() { }
-
+  @HostListener("click") onClicks() {
+    this.highlight("highlight")
+  }
+  @HostListener("dblclick") onDoubleClicks() {
+    this.highlight("None")
+  }
+  private highlight(highlight: string){
+    this.elem.nativeElement.style.backgroundColor = 'yellow';
+  }
 }
