33ec2861d1366e6c1d5d43c0ab3b17bd4d0f5f20
Tom Chege <emdeechege@gmail.com>
2018-08-12 00:20:05 +0300

    styling of cards

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -6,6 +6,7 @@ public showDescription: boolean;
     public id: number,
     public name: string,
     public description: string,
+    public submitter: string,
     public completeDate: Date)
     {
         this.showDescription = false
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
 quotes=[
-  new Quote(1, 'Nelson Mandela', 'There is no passion to be found playing small - in settling for a life that is less than the one you are capable of living.', new Date(1990, 5, 10)),
-  new Quote(2, 'Mohandas Gandhi', 'Strength does not come from physical capacity. It comes from an indomitable will.', new Date(1950, 3, 23)),
-  new Quote(3, 'Rick Flores', 'We have to come for them before they come for us, it is that simple', new Date(2012, 8, 17)),
+  new Quote(1, 'Nelson Mandela', 'There is no passion to be found playing small - in settling for a life that is less than the one you are capable of living.','admin', new Date(1990, 5, 10)),
+  new Quote(2, 'Mohandas Gandhi', 'Strength does not come from physical capacity. It comes from an indomitable will.','admin', new Date(1950, 3, 23)),
+  new Quote(3, 'Rick Flores', 'We have to come for them before they come for us, it is that simple','admin', new Date(2012, 8, 17)),
 
 ]
 
@@ -24,7 +24,7 @@ this.quotes.push(quote)
 
 deleteQuote(isComplete, index) {
   if (isComplete) {
-    let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+    let toDelete = confirm(`Do you want to delete this Quote? ${this.quotes[index].name}`)
 
     if (toDelete) { this.quotes.splice(index, 1) }
   }
